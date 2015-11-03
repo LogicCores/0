8931260e1678c864c8dfa15cca5006c7812099f0
Christoph Dorn <christoph@christophdorn.com>
2015-11-02 22:48:46 -0800

    wip

--- www/page.js
+++ www/page.js
@@ -64,22 +64,11 @@ function bootForLib (LIB) {
 		return LIB.Promise.try(function () {
 
 			contexts.auth.on("changed:authenticated", function (authenticated) {
-
 				if (authenticated) {
 					contexts.page.setViews([
 						"loggedin",
 						"logout"
 					]);
-
-					$('#community-menu').visibility({
-						type   : 'fixed',
-						offset : 0
-					});
-
-if (typeof window.animateSkin === "function") {
-	window.animateSkin();
-}
-
 				} else {
 					contexts.page.setViews([
 						"loggedout",
@@ -275,45 +264,6 @@ if (typeof window.animateSkin === "function") {
 				});
 			});
 
-
-			var mainMenuPinned = null;
-
-			contexts.page.on("changed:path", function (path) {
-
-// TODO: Move this into the admin skin.
-
-				try {
-					// TODO: Optionally remember scoll positions of pages and re-apply on nav.
-					var menuHeight = $(".main.menu").height();
-					var pageContentElm = $("#page-content");
-					var pageContentY = pageContentElm.offset().top;
-					if (
-						mainMenuPinned ||
-						/\/Community\//.test(path)
-					) {
-						$(document).scrollTop(pageContentY - menuHeight * 2);
-					}
-					$('.main.menu .item').removeClass("active");
-					$('.main.menu .item[href="' + path + '"]').addClass("active");
-				} catch (err) {
-					console.error("suppressed error", err);
-				}
-			});
-	
-			var onScroll = LIB._.debounce(function () {
-				var isPinned = $(".main.menu").hasClass("fixed");
-				if (isPinned !== mainMenuPinned) {
-					mainMenuPinned = isPinned;
-					if (mainMenuPinned) {
-						$("body").addClass("main-menu-pinned");
-					} else {
-						$("body").removeClass("main-menu-pinned");
-					}
-				}
-			}, 100);
-			$(document).on('scroll', onScroll);
-			onScroll();
-	
 		}).catch(function (err) {
 			console.error("Error initializing page management:", err.stack);
 			throw err;
