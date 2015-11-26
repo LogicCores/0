70dfcdbf449a6687670756cfee73a11228db6541
Christoph Dorn <christoph@christophdorn.com>
2015-11-26 15:46:22 -0800

    wip

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -10,25 +10,30 @@ function init {
 	BO_deriveSelfDir ___TMP___ "$BO_SELF_BASH_SOURCE"
 	local __BO_DIR__="$___TMP___"
 
-	
+
     BO_sourcePrototype "$__BO_DIR__/activate.sh"
 
 
-	function Bundle {
-		BO_format "$VERBOSE" "HEADER" "Bundling system"
+	function BundleDependencies {
+		BO_format "$VERBOSE" "HEADER" "Bundling dependencies for system"
 
 		BO_log "$VERBOSE" "PWD: $PWD"
 
+	    BO_sourcePrototype "$Z0_ROOT/lib/node.pack/node.pack.proto.sh"
+
+		# Pack the dependencies into an archive and upload if not already uploaded.
+		node.pack "dependencies"
+
+# TODO: Enable this again to bundle deps in a flat way with Node 5 & npm 3.
 # TODO: Enable updating of zero.lib based on user config
 #			"lib/smi.0/bin/smi.0" freeze-latest --library-package "lib/zero.lib"
-
 # TODO: Use 'sm.expand' to manage submodules and checked out branches.
 #			"lib/smi.0/bin/smi.0" freeze-latest
 
 		BO_format "$VERBOSE" "FOOTER"
 	}
 
-	Bundle $@
+	BundleDependencies $@
 
 }
 init $@
